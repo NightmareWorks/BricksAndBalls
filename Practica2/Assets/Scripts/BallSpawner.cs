2f675b62df4513888dc1a05d880430e89767329a
DavidGonzalezJ <davidgonzalezj97@gmail.com>
2018-11-29 21:30:16 +0100

    Bloques con puntos y spawn de bola

---
+++ Practica2/Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+    public GameObject ball;
+    public int numBalls;
+    // Use this for initialization
+    void Start () {
+        //ball1 = Instantiate(ball);
+        //ball2 = Instantiate(ball);
+        numBalls = 10;
+        StartCoroutine(spawnBalls());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Intento chusco
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            //ball1.GetComponent<Ball>().startMove();
+        }
+        else if (Input.GetKeyUp(KeyCode.V))
+        {
+            //ball2.GetComponent<Ball>().startMove();
+        }
+    }
+
+    IEnumerator spawnBalls()
+    {
+        for (int i = 0; i < numBalls; i++)
+        {
+            GameObject actB = Instantiate(ball);
+            actB.GetComponent<Ball>().startMove();
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+
+}
---
+++ Practica2/Assets/Scripts/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour {
+
+    private int life;
+
+    [Tooltip("Vida")]
+    public Text txt;
+
+	// Use this for initialization
+	void Start () {
+        life = 5;
+        txt.text = life.ToString();
+	}
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        --life;
+        if (life <= 0) {
+            Destroy(gameObject);
+        }
+        else
+            txt.text = life.ToString();
+    }
+}
