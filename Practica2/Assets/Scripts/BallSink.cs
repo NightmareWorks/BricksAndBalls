97572257ce23bef06648727733e972439585b4ee
Patricia Cabrero <patrcabr@ucm.es>
2019-04-13 16:58:26 +0200

    Arreglado menu escalado

--- Practica2/Assets/Scripts/BallSink.cs
+++ Practica2/Assets/Scripts/BallSink.cs
@@ -46,7 +46,7 @@ public class BallSink : MonoBehaviour {
     //Sets it
     public void setPos(float x, float y) {
         pos = new Vector2(x, y+size.y);
-        fakeBall.gameObject.transform.position = new Vector3(pos.x, pos.y, 0);
+        //fakeBall.gameObject.transform.position = new Vector3(pos.x, pos.y, 0);
     }
 
     public void setPosX(float x)
