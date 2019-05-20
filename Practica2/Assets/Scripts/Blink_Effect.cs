eb34b7ff42d6289114a8cf4158ecb9c544ac9466
Patricia Cabrero <patrcabr@ucm.es>
2019-05-21 00:39:01 +0200

    Hecho downgrade a unity antiguo

---
+++ Practica2/Assets/Scripts/Blink_Effect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink_Effect : MonoBehaviour
+{
+    public static bool visible;
+     private void Start() {
+        visible = true;
+        StartCoroutine(BlinkCo());
+     }
+    IEnumerator BlinkCo() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        while (visible) {
+            yield return new WaitForSeconds(1.0f);
+            Color c = sr.color;
+            float saveAlpha = c.a;
+            c.a = 0.8f;
+            sr.color = c;
+
+            yield return new WaitForSeconds(0.2f);
+            c.a = 0.5f;
+            sr.color = c;
+
+            yield return new WaitForSeconds(0.2f);
+            c.a = 0.8f;
+            sr.color = c;
+
+            yield return new WaitForSeconds(0.2f);
+
+            c.a = saveAlpha;
+            sr.color = c;
+        }
+    }
+}
